20fd795c4bdc29cad751587ec4fcbce9bb0fce0a
Joe Olliff <joe_olliff@rogers-brown.com>
2016-06-27 20:39:09 +0000

    hopefully final

--- server.js
+++ server.js
@@ -2,13 +2,9 @@
 
 var express = require('express');
 var routes = require('./app/routes/routes.js');
-var mongoose = require('mongoose');
 var session = require('express-session');
 var dateCheck = require('./app/controllers/timestampController.js');
 var app = express();
-require('dotenv').load();
-
-mongoose.connect(process.env.MONGO_URI);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
